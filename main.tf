a18167fe976006af9c6165bf32219f39b1cba3e2
mandspritchard <mandspritchard@googlemail.com>
2026-06-11 10:30:13 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -53,7 +53,7 @@ module "blog_sg" {
       to_port     = 80
       protocol    = "tcp"
       description = "HTTP"
-      cidr_ipv4   = ["0.0.0.0/0"]
+      cidr_ipv4   = "0.0.0.0/0"
     }
 
     https = {
@@ -61,17 +61,17 @@ module "blog_sg" {
       to_port     = 443
       protocol    = "tcp"
       description = "HTTPS"
-      cidr_ipv4   = ["0.0.0.0/0"]
+      cidr_ipv4   = "0.0.0.0/0"
     }
   }
 
   egress_rules = {
-    {
+    all =  {
       from_port   = 0
       to_port     = 0
       protocol    = "-1"
       description = "Allow all outbound"
-      cidr_ipv4   = ["0.0.0.0/0"]
+      cidr_ipv4   = "0.0.0.0/0"
     }
   }
 }
