eea8b8d8ca12e1fb1e9c6789969cec1f6aeff4b2
mandspritchard <mandspritchard@googlemail.com>
2026-06-11 12:27:48 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
+output "sg_debug" {
+  value = module.blog_sg
+}
+
+
+
 data "aws_ami" "app_ami" {
   most_recent = true
 
@@ -32,7 +38,7 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids = [module.blog_sg.security_group_id != null ? module.blog_sg.security_group_id : null]
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
     Name = "HelloWorld"
