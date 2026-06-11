edb0fcb6cf2b6c0d7c5ed2a0dadbd93324968931
mandspritchard <mandspritchard@googlemail.com>
2026-06-11 10:41:47 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
+  vpc_security_group_ids = [module.blog_sg.this_security_group_id]
 
   tags = {
     Name = "HelloWorld"
